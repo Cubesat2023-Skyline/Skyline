0d80aba3d451d0ff5e1aa825bfbb198ec82c105c
FIFA AK <pataradany@gmail.com>
2024-01-04 15:13:13 +0700

    function done

--- calculate_area_from_height.cpp
+++ calculate_area_from_height.cpp
@@ -1,8 +1,17 @@
+//from altitude
+// wide_x=66deg ,wide_y=41deg
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+double deg(double degree){
+     double PI = 3.14159265359;
+    return degree*(PI/180.0);
+}
+main(){
     cin.sync_with_stdio(0);cin.tie(0);
-    cout << "test";
-
+    double altitude;cin>>altitude;
+    double real_height = altitude*tan(deg(41))*2;
+    double real_width = altitude*tan(deg(66))*2;
+    string ans = to_string(real_height*real_width/1000000);
+    cout << ans;
     return 0;
 }
