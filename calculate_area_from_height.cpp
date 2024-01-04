bcb33654140e28c60ac4377053e94894d2b274f7
FIFA AK <pataradany@gmail.com>
2024-01-04 15:29:45 +0700

    added some comment

--- calculate_area_from_height.cpp
+++ calculate_area_from_height.cpp
@@ -1,17 +1,22 @@
 //from altitude
-// wide_x=66deg ,wide_y=41deg
+// wide_x=66deg ,wide_y=41deg <- camera spec
 #include <bits/stdc++.h>
 using namespace std;
+
 double deg(double degree){
      double PI = 3.14159265359;
     return degree*(PI/180.0);
 }
-main(){
-    cin.sync_with_stdio(0);cin.tie(0);
-    double altitude;cin>>altitude;
+
+string calculate(double altitude){
     double real_height = altitude*tan(deg(41))*2;
     double real_width = altitude*tan(deg(66))*2;
-    string ans = to_string(real_height*real_width/1000000);
-    cout << ans;
+    string ans = to_string(real_height*real_width/1000000); //convert to km in string
+    return ans;
+}
+main(){
+    cin.sync_with_stdio(0);cin.tie(0); //just optimize
+    double altitude;cin>>altitude; // m
+    cout << calculate(altitude) << " km^2";
     return 0;
 }
