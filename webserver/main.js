abddda6099a065e57cfa279536d23753e2f45e44
FIFA AK <pataradany@gmail.com>
2024-01-07 16:25:57 +0700

    added safety condition

--- webserver/main.js
+++ webserver/main.js
@@ -5,6 +5,88 @@ const gps = new GPS_module();
 
 
 
+//switch
+const bme_switch = document.getElementById("bme_switch");
+const gps_switch = document.getElementById("gps_switch");
+let bme_state = false;
+let gps_state = false;
+bme_switch.addEventListener("change",()=>{
+    if (bme_switch.checked){
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                socket.send("tc;3063;1;0");
+                bme_state = true;
+            } 
+            else {
+                alert("WebSocket connection is not open.");
+                bme_switch.checked = !bme_switch.checked;
+            }
+    }
+    else{
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                socket.send("tc;3063;2;0");
+                bme_state = false;
+            } 
+            else {
+                alert("WebSocket connection is not open.");
+                bme_switch.checked = !bme_switch.checked;
+
+            }
+    }
+})
+
+gps_switch.addEventListener("change",()=>{
+    if (gps_switch.checked){
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                socket.send("tc;3022;1;0");
+                gps_state = true;
+            } 
+            else {
+                alert("WebSocket connection is not open.");
+
+                gps_switch.checked = !gps_switch.checked;
+
+            }
+    }
+    else{
+            if (socket && socket.readyState === WebSocket.OPEN) {
+                socket.send("tc;3022;2;0");
+                gps_state = false;
+            } 
+            else {
+
+                alert("WebSocket connection is not open.");
+                gps_switch.checked = !gps_switch.checked;
+
+            }
+    }
+})
+
+const get_value_from_bme = document.getElementById("get_value_from_bme");
+const get_value_from_gps = document.getElementById("get_value_from_gps");
+
+get_value_from_bme.addEventListener("click",()=>{
+    if (bme_state){
+        socket.send("tm;3063;3");
+    }
+    else{
+        alert("bme280 not working");
+    }
+})
+
+get_value_from_gps.addEventListener("click",()=>{
+    if (gps_state){
+        socket.send("tm;3022;5");
+    }
+    else{
+        alert("gps not working");
+    }
+})
+
+
+
+
+
+
 
 // SERVER HANDLE
 
