fcd5ae5c7ad7fd9218a86b4534ca25a321dcf5a2
FIFA AK <pataradany@gmail.com>
2024-01-12 11:45:43 +0700

    dixed errors

--- webserver/Handle_log.js
+++ webserver/Handle_log.js
@@ -1,52 +1,125 @@
-export function handleLogs() {
+// Handle_log.js
+class BME_log {
+  constructor(bme) {
+    this.bme = bme;
+  }
+
+  handleLogs(match) {
+    let data = match;
     let csvData = ''; // Accumulated CSV data
     const getValueFromBMEButton = document.getElementById('get_value_from_bme');
     const bmeLogButton = document.getElementById('bme_log');
     let header_state = false;
 
-    getValueFromBMEButton.addEventListener('click', function () {
-        const bmeData = get_value_from_bme();
-        appendToCSV(bmeData);
+    getValueFromBMEButton.addEventListener('click', () => {
+      const bmeData = this.get_value_from_bme(data);
+      this.appendToCSV(bmeData);
     });
 
-    bmeLogButton.addEventListener('click', function () {
-        downloadCSV();
-        header_state = false;
+    bmeLogButton.addEventListener('click', () => {
+      this.downloadCSV();
+      header_state = false;
     });
 
-    function appendToCSV(data) {
-        const csvLine = convertToCSV(data);
-        csvData += csvLine;
-    }
+    this.appendToCSV = (data) => {
+      const csvLine = this.convertToCSV(data);
+      csvData += csvLine;
+    };
 
-    function downloadCSV() {
-        const blob = new Blob([csvData], { type: 'text/csv' });
-        const a = document.createElement('a');
-        a.href = URL.createObjectURL(blob);
-        const timestamp = new Date().toISOString().replace(/:/g, '-');
-        a.download = `bme_data_${timestamp}.csv`;
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-    }
+    this.downloadCSV = () => {
+      const blob = new Blob([csvData], { type: 'text/csv' });
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(blob);
+      const timestamp = new Date().toISOString().replace(/:/g, '-');
+      a.download = `bme_data_${timestamp}.csv`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    };
 
-    function convertToCSV(data) {
-        const header = Object.keys(data).join(',');
-        const values = Object.values(data).join(',');
-        if (header_state) {
-            return `${values}\n`;
-        } else {
-            header_state = true;
-            return `${header}\n${values}\n`;
-        }
-    }
+    this.convertToCSV = (data) => {
+      const header = Object.keys(data).join(',');
+      const values = Object.values(data).join(',');
+      if (header_state) {
+        return `${values}\n`;
+      } else {
+        header_state = true;
+        return `${header}\n${values}\n`;
+      }
+    };
 
-    function get_value_from_bme() {
-        const new_data = parseFloat(match[0].replace("(", "").replace(")", "")).toFixed(4);
-        const new_data2 = bme.convert_raw2engineering_data(new_data);
-        return {
-            ALTITUDE: bme.calculate_altitude(new_data2),
-            AREA: bme.calculate_area(new_data2)
-        };
+    this.get_value_from_bme = (data) => {
+      const new_data = parseFloat(data[0].replace("(", "").replace(")", "")).toFixed(4);
+      const new_data2 = this.bme.convert_raw2engineering_data(new_data);
+      return {
+        ALTITUDE: this.bme.calculate_altitude(new_data2),
+        AREA: this.bme.calculate_area(new_data2)
+      }
     }
+  }
 }
+
+
+
+
+class GPS_log {
+  constructor(gps) {
+    this.gps = gps;
+  }
+
+  handleLogs(match) {
+    let csvData = ''; // Accumulated CSV data
+    const getValueFromgpsButton = document.getElementById('get_value_from_gps');
+    const gpsLogButton = document.getElementById('gps_log');
+    let header_state = false;
+
+    getValueFromgpsButton.addEventListener('click', () => {
+      const gpsData = this.get_value_from_gps(match);
+      this.appendToCSV(gpsData);
+    });
+
+    gpsLogButton.addEventListener('click', () => {
+      this.downloadCSV();
+      header_state = false;
+    });
+
+    this.appendToCSV = function (data) {
+      const csvLine = this.convertToCSV(data);
+      csvData += csvLine;
+    };
+
+    this.downloadCSV = function () {
+      const blob = new Blob([csvData], { type: 'text/csv' });
+      const a = document.createElement('a');
+      a.href = URL.createObjectURL(blob);
+      const timestamp = new Date().toISOString().replace(/:/g, '-');
+      a.download = `gps_data_${timestamp}.csv`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    };
+
+    this.convertToCSV = function (data) {
+      const header = Object.keys(data).join(',');
+      const values = Object.values(data).join(',');
+      if (header_state) {
+        return `${values}\n`;
+      } else {
+        header_state = true;
+        return `${header}\n${values}\n`;
+      }
+    };
+
+    // Added missing curly braces `{`
+    this.get_value_from_gps = () => {
+      const new_data = parseFloat(match[0].replace("(", "").replace(")", "")).toFixed(4);
+      const new_data2 = this.gps.convert_raw2engineering_data(new_data);
+      return {
+        ALTITUDE: this.gps.calculate_altitude(new_data2),
+        AREA: this.gps.calculate_area(new_data2)
+      };
+    };
+  }
+}
+
+export { BME_log, GPS_log };
--- webserver/main.js
+++ webserver/main.js
@@ -1,11 +1,12 @@
 import BME_module from './BME.js'; 
 import GPS_module from './GPS.js';
-import { handleLogs } from './Handle_log.js';
 import { Handle_module_switch} from './Module_switch.js';
 import { server_status } from './Server_status.js';
-
+import {BME_log,GPS_log} from './Handle_log.js';
 const bme = new BME_module();
 const gps = new GPS_module();
+const bme_log = new BME_log(bme);
+const gps_log = new GPS_log(gps);
 let socket;
 let bme_state;
 let gps_state;
@@ -22,12 +23,13 @@ function connectWebSocket(url) {
         if (match){
             const new_data = parseFloat(match[0].replace("(","").replace(")","")).toFixed(4); //converbt raw data to engineering data
             if (event.data.substring(0,8)=="TM;3063;"){ //bme detection
-
+            bme_log.handleLogs(match); // Call the function from Handle_log.js
             const new_data2 = bme.convert_raw2engineering_data(new_data);
             console.log("altitude: ",bme.calculate_altitude(new_data2),"m");
             console.log("Area: ",bme.calculate_area(new_data2)," km^2");
             }
             else if (event.data.substring(0,8)=="TM;3022;"){//gps detection
+                // gps_log.handleLogs(match); // Call the function from Handle_log.js
                 console.log("altitude: ",gps.convert_raw2engineering_data(new_data),"m");
                 console.log("Area: ",gps.calculate_area(new_data)," km^2");
             }
@@ -46,8 +48,6 @@ document.getElementById("messageForm").addEventListener("submit", (event) => {
     }
     messageInput.value = "";
 });
-
-
 // Call the function from Module_switch.js
 Handle_module_switch(socket,  (newState) => {bme_state = newState;},  (newState) => {gps_state = newState;});
 const get_value_from_bme = document.getElementById("get_value_from_bme");
@@ -56,22 +56,14 @@ get_value_from_bme.addEventListener("click",()=>{
     if (bme_state){
         socket.send("tm;3063;3");
     }
-    else{
-        alert("bme280 not working");
-    }
+    else{alert("bme280 not working");}
 })
 get_value_from_gps.addEventListener("click",()=>{
     if (gps_state){
         socket.send("tm;3022;5");
     }
-    else{
-        alert("gps not working");
-    }
+    else{alert("gps not working");}
 })
-// Handle log
-// document.addEventListener('DOMContentLoaded', function () {
-//     handleLogs(); // Call the function from Handle_log.js
-// });
 document.getElementById("connectButton").addEventListener("click", () => {
    const serverUrl = document.getElementById("serverUrl").value;
    if (serverUrl.trim()!=' ') {connectWebSocket(serverUrl); }
